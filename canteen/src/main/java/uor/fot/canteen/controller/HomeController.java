d1eaa00343d815edd7301dfee78228fbcab3338c
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-03 03:37:13 +0530

    ad sql file

--- canteen/src/main/java/uor/fot/canteen/controller/HomeController.java
+++ canteen/src/main/java/uor/fot/canteen/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String viewHomePage(Model model){
-        return "login.html";
+        return "admin/admin_dashboard.html";
     }
 
     @RequestMapping("/admindashboard")
