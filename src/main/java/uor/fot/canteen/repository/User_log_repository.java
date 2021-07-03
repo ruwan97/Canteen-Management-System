0b5d39e8b046a060a3831dc7a172963fbc8746ba
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-03 21:17:50 +0530

    delete error file

--- src/main/java/uor/fot/canteen/repository/User_log_repository.java
+++
@@ -1,7 +0,0 @@
-package uor.fot.canteen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uor.fot.canteen.model.User_log;
-
-public interface User_log_repository extends JpaRepository<User_log, Integer> {
-}
