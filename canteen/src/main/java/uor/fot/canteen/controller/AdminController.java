35deba15dbb2decf70d437d6b2d562e6037ff95d
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-04 01:46:55 +0530

    mapping web page

--- canteen/src/main/java/uor/fot/canteen/controller/AdminController.java
+++ canteen/src/main/java/uor/fot/canteen/controller/AdminController.java
@@ -7,113 +7,113 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-    @RequestMapping("/admindashboard")
+    @RequestMapping("/home/login/admindashboard")
     public String viewAdminDash(Model model){
         return "admin/admin_dashboard";
     }
 
     //user
-    @RequestMapping("/admindashboard/user/view")
+    @RequestMapping("/home/login/admindashboard/user/view")
     public String viewUsers(Model model){
         return "admin/users";
     }
 
-    @RequestMapping("/admindashboard/user/add")
+    @RequestMapping("/home/login/admindashboard/user/add")
     public String viewUserAdd(Model model){
         return "admin/user_add";
     }
 
-    @RequestMapping("/admindashboard/user/update")
+    @RequestMapping("/home/login/admindashboard/user/update")
     public String viewUserUpdate(Model model){
         return "admin/user_update";
     }
 
-    @RequestMapping("/admindashboard/user/updateform")
+    @RequestMapping("/home/login/admindashboard/user/updateform")
     public String viewUserUpdateForm(Model model){
         return "admin/user_update_form";
     }
 
     //item
-    @RequestMapping("/admindashboard/items/view")
+    @RequestMapping("/home/login/admindashboard/items/view")
     public String viewItems(Model model){
         return "admin/items";
     }
 
-    @RequestMapping("/admindashboard/item/add")
+    @RequestMapping("/home/login/admindashboard/item/add")
     public String viewItemAdd(Model model){
         return "admin/item_add";
     }
 
-    @RequestMapping("/admindashboard/item/update")
+    @RequestMapping("/home/login/admindashboard/item/update")
     public String viewItemUpdate(Model model){
         return "admin/item_update";
     }
 
-    @RequestMapping("/admindashboard/item/updateform")
+    @RequestMapping("/homelogin/admindashboard/item/updateform")
     public String viewItemUpdateForm(Model model){
         return "admin/item_update_form";
     }
 
     //inventory
-    @RequestMapping("/admindashboard/inventory/view")
+    @RequestMapping("/home/login/admindashboard/inventory/view")
     public String viewInventory(Model model){
         return "admin/inventory";
     }
 
-    @RequestMapping("/admindashboard/inventory/add")
+    @RequestMapping("/home/login/admindashboard/inventory/add")
     public String viewInventoryAdd(Model model){
         return "admin/inventory_add";
     }
 
-    @RequestMapping("/admindashboard/inventory/update")
+    @RequestMapping("/login/admindashboard/inventory/update")
     public String viewInventoryUpdate(Model model){
         return "admin/inventory_update";
     }
 
-    @RequestMapping("/admindashboard/inventory/updateform")
+    @RequestMapping("/home/login/admindashboard/inventory/updateform")
     public String viewInventoryUpdateForm(Model model){
         return "admin/inv_update_form";
     }
 
     //order
-    @RequestMapping("/admindashboard/orders/view")
+    @RequestMapping("/home/login/admindashboard/orders/view")
     public String viewOrders(Model model){
         return "admin/orders";
     }
 
     //transaction
-    @RequestMapping("/admindashboard/transaction/view")
+    @RequestMapping("/home/login/admindashboard/transaction/view")
     public String viewTransactions(Model model){
         return "admin/transactions";
     }
 
-    @RequestMapping("/admindashboard/transaction/viewsummary")
+    @RequestMapping("/home/login/admindashboard/transaction/viewsummary")
     public String viewTransactionsSummary(Model model){
         return "admin/transactions_summary";
     }
 
     //logs
-    @RequestMapping("/admindashboard/logs/userlog/view")
+    @RequestMapping("/home/login/admindashboard/logs/userlog/view")
     public String viewUserLog(Model model){
         return "admin/user_log";
     }
 
-    @RequestMapping("/admindashboard/logs/oredrlog/view")
+    @RequestMapping("/home/login/admindashboard/logs/oredrlog/view")
     public String viewOrderLog(Model model){
         return "admin/order_log";
     }
 
-    @RequestMapping("/admindashboard/logs/itemlog/view")
+    @RequestMapping("/home/login/admindashboard/logs/itemlog/view")
     public String viewItemLog(Model model){
         return "admin/item_log";
     }
 
-    @RequestMapping("/admindashboard/logs/inventorylog/view")
+    @RequestMapping("/home/login/admindashboard/logs/inventorylog/view")
     public String viewInventoryLog(Model model){
         return "admin/inventory_log";
     }
 
-    @RequestMapping("/admindashboard/logs/transactionlog/view")
+    @RequestMapping("/home/login/admindashboard/logs/transactionlog/view")
     public String viewTransactionLog(Model model){
         return "admin/transaction_log";
     }
