997ddd9ddb011de8ef5e1103408cf3cc60a1d386
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-03 20:33:37 +0530

    delete error files

--- canteen/src/main/java/uor/fot/canteen/repository/Transaction_repository.java
+++
@@ -1,7 +0,0 @@
-package uor.fot.canteen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uor.fot.canteen.model.Transaction;
-
-public interface Transaction_repository extends JpaRepository<Transaction, Integer> {
-}
