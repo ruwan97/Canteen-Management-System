55ea0623ad18204bd5679e4c7995232d5cb5f341
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-03 05:57:49 +0530

    update home controller

--- canteen/src/main/java/uor/fot/canteen/controller/HomeController.java
+++ canteen/src/main/java/uor/fot/canteen/controller/HomeController.java
@@ -1,9 +1,12 @@
 package uor.fot.canteen.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
+
     @RequestMapping("/")
     public String viewHomePage(Model model){
         return "home/index";
