1433c5a2042c4815138bcd2533994d193540a83e
ruwan97 <61679180+ruwan97@users.noreply.github.com>
2021-07-04 17:46:26 +0530

    register user

--- canteen/src/main/java/uor/fot/canteen/controller/User_controller.java
+++ canteen/src/main/java/uor/fot/canteen/controller/User_controller.java
@@ -12,14 +12,14 @@ public class User_controller {
     private User_service user_service;
 
     //create user
-    @PostMapping("/saveUser")
+    @PostMapping("/home/saveuser")
     public String addUser(@RequestParam("id") String u_id, @RequestParam("name") String u_name, @RequestParam("email") String u_email, @RequestParam("password") String u_password, @RequestParam("role") String u_role, @RequestParam("contact") String u_contact, @RequestParam("image") String u_image)
     {
         boolean result = user_service.addUser(u_id, u_name, u_email,u_password, u_role, u_contact, u_image);
         if (result)
-            return "redirect:/";
+            return "redirect:/home/login";
         else
-            return "redirect:/register";
+            return "redirect:/home/register";
     }
 
     //view user
